ed916cff36da20d9f7ee4703833cf0cf14abb630
Robin Nilsson <robinonilsson@gmail.com>
2014-11-24 11:19:59 +0100

    Changing message

--- MoveInfo.cs
+++ MoveInfo.cs
@@ -40,7 +40,8 @@ namespace ConsoleChessGame
 
             }
             else
-                return string.Format("{0} moved from {1}, {2}, to {3},{4}", ally.type, OldX, OldY, ally.position.X, ally.position.Y);
+                return string.Format("{0} moved from {1}, {2}, to {3},{4}", ally.type, OldX, OldY, ally.position.X, ally.position.Y
+                    );
         }
     }
 }
